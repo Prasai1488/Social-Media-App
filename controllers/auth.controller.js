1982300faae5df9b42f3744bab18d2bf7f26564d
Prasai1488 <prasaiprithvi4@gmail.com>
2024-08-15 19:22:32 +0545

    loginRelate

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
 import prisma from '../lib/prisma.js'
+import  jwt  from 'jsonwebtoken'
 
+// ? Function to register new user
 export const register = async (req, res) => {
   // extract user details from req.body
   const newUser = req.body
@@ -22,17 +24,65 @@ export const register = async (req, res) => {
     //   update new users password with hashed password
     newUser.password = hashedPassword
 
-      // Set default role as 'user' (or 'admin' as needed)
-      newUser.role = 'user';
+    // Set default role as 'user' (or 'admin' as needed)
+    newUser.role = 'user'
 
     //   save the user into the database
     await prisma.user.create({
-      data:  newUser ,
+      data: newUser,
     })
 
     res.status(201).send({ message: 'User created successfully' })
   } catch (error) {
     console.log(error)
     res.status(500).send({ message: 'Failed to create user!' })
+  }
+}
+
+// ? Function to login user into the system
+export const login = async (req, res) => {
+  // extract login credentials from req.body
+  const { username, password } = req.body
+
+  try {
+    //   check if username is valid/exists in the database
+    const user = await prisma.user.findUnique({
+      where: { username },
+    })
+    if (!user) {
+      return res.status(401).send({ message: 'Invalid credentials' })
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+
+    if (!isValidPassword) {
+      return res.status(401).send({ message: 'Invalid credentials' })
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7
+    const token = jwt.sign(
+      {
+        id : user.id,
+        username: user.username,
+        
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    )
+
+    // Extract user information excluding password
+    const { password: userPassword, ...userInfo } = user
+
+    // Set the token in an HTTP-only cookie and send the response
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+        maxAge: age,
+      })
+      .status(200)
+      .send(userInfo)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: 'Failed to login!!' })
   }
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,9 +2,12 @@ import express from 'express'
 import { registerValidationSchema } from '../validationSchema/register.login.validation.js'
 import { register } from '../controllers/auth.controller.js'
 import {validateReqBody} from "../middleware/validation.middleware.js"
+import { login } from '../controllers/auth.controller.js'
+import { loginValidationSchema } from '../validationSchema/register.login.validation.js'
 
 const router = express.Router()
 
 router.post('/register',validateReqBody(registerValidationSchema), register)
+router.post('/login',validateReqBody(loginValidationSchema),login)
 
 export default router
